97da196af08b159ebbdde46eeaf4eed0688dcc9e
Greg Frasco <frascog@wit.edu>
2016-10-03 21:40:35 -0400

    Removed Unused Imports

--- src/main/java/com/gregfrasco/yahoofinanceapi/StockFetcher.java
+++ src/main/java/com/gregfrasco/yahoofinanceapi/StockFetcher.java
@@ -7,11 +7,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Date;
-import java.util.Iterator;
-import java.util.Set;
-import java.util.TreeSet;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
